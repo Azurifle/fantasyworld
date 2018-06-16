e4f4077b7725b20f74acb6b6347314345673bc63
Nin Azurifle <dsirikasem@hotmail.com>
2018-06-16 16:01:44 +0700

    Vec2i has many operators
    not finish yet

--- Week_9/Vec2_i.cpp
+++ Week_9/Vec2_i.cpp
@@ -11,6 +11,11 @@ namespace G6037599
 
     print(v1);
     print(v2);
+
+    if(v1 - v2 == Vec2_i(-4, 4))
+    {
+      puts("Pass");
+    }
   }
 
   void print(const Vec2_i& t_vector)
@@ -18,6 +23,111 @@ namespace G6037599
     std::cout << '(' << t_vector.x << ',' << t_vector.y << ')' << std::endl;
   }
 
+  const Vec2_i Vec2_i::ZEROS = { 0, 0 };
+  const Vec2_i Vec2_i::ONES = { 1, 1 };
+
   //___ (de)constructors/operators ____________________________________
   Vec2_i::Vec2_i(const int t_x, const int t_y) : x(t_x), y(t_y) {}
+
+  Vec2_i Vec2_i::operator+=(const Vec2_i& t_other)
+  {
+    return { x += t_other.x, y += t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator-=(const Vec2_i& t_other)
+  {
+    return { x -= t_other.x, y -= t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator*=(const Vec2_i& t_other)
+  {
+    return { x *= t_other.x, y *= t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator/=(const Vec2_i& t_other)
+  {
+    return { x /= t_other.x, y /= t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator<<(const Vec2_i& t_other) const
+  {
+    return { x << t_other.x, y << t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator+(const Vec2_i& t_other) const
+  {
+    return { x + t_other.x, y + t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator-(const Vec2_i& t_other) const
+  {
+    return { x - t_other.x, y - t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator*(const Vec2_i& t_other) const
+  {
+    return { x * t_other.x, y * t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator/(const Vec2_i& t_other) const
+  {
+    return { x / t_other.x, y / t_other.y };
+  }
+
+  Vec2_i Vec2_i::operator*(const float t_scalar) const
+  {
+    return { x * t_scalar, y * t_scalar };
+  }
+
+  Vec2_i Vec2_i::operator/(const float t_scalar) const
+  {
+    return { x / t_scalar, y / t_scalar };
+  }
+
+  bool Vec2_i::operator<=(const Vec2_i& t_other) const
+  {
+    return x <= t_other.x && y <= t_other.y;
+  }
+
+  bool Vec2_i::operator>=(const Vec2_i& t_other) const
+  {
+    return x >= t_other.x && y >= t_other.y;
+  }
+
+  bool Vec2_i::operator==(const Vec2_i& t_other) const
+  {
+    return x == t_other.x && y == t_other.y;
+  }
+
+  bool Vec2_i::operator!=(const Vec2_i& t_other) const
+  {
+    return x != t_other.x && y != t_other.y;
+  }
+
+  bool Vec2_i::operator<(const Vec2_i& t_other) const
+  {
+    return x < t_other.x && y < t_other.y;
+  }
+
+  bool Vec2_i::operator>(const Vec2_i& t_other) const
+  {
+    return x > t_other.x && y > t_other.y;
+  }
+
+  //___ public _______________________________________________________
+  int Vec2_i::dot_product(const Vec2_i& t_other) const
+  {
+    return x*t_other.x + y*t_other.y;
+  }
+
+  Vec2_i Vec2_i::closs_product(const Vec2_i& t_other) const
+  {
+    return Vec2_i{ y - t_other.y, x - t_other.x };
+  }
+
+  int Vec2_i::get_magnitude() const
+  {
+    return sqrt(x*x + y*y);
+  }
+
 }//G6037599
--- Week_9/Vec2_i.hpp
+++ Week_9/Vec2_i.hpp
@@ -7,16 +7,41 @@ namespace G6037599
   class Vec2_i final
   {
   public:
+    static const Vec2_i ZEROS, ONES;
+
     int x, y;//if there is no setter constraint make it public
 
     Vec2_i() = default;
     ~Vec2_i() = default;
     Vec2_i(int t_x, int t_y);
-  private:
     Vec2_i(const Vec2_i& t_to_copy) = default;
     Vec2_i(Vec2_i&& t_to_move) noexcept = default;
     Vec2_i& operator = (const Vec2_i& t_to_copy) = default;
     Vec2_i& operator = (Vec2_i&& t_to_move) noexcept = default;
+
+    Vec2_i operator += (const Vec2_i& t_other);
+    Vec2_i operator -= (const Vec2_i& t_other);
+    Vec2_i operator *= (const Vec2_i& t_other);
+    Vec2_i operator /= (const Vec2_i& t_other);
+
+    Vec2_i operator << (const Vec2_i& t_other) const;
+    Vec2_i operator + (const Vec2_i& t_other) const;
+    Vec2_i operator - (const Vec2_i& t_other) const;
+    Vec2_i operator * (const Vec2_i& t_other) const;
+    Vec2_i operator / (const Vec2_i& t_other) const;
+    Vec2_i operator * (float t_scalar) const;
+    Vec2_i operator / (float t_scalar) const;
+
+    bool operator <= (const Vec2_i& t_other) const;
+    bool operator >= (const Vec2_i& t_other) const;
+    bool operator == (const Vec2_i& t_other) const;
+    bool operator != (const Vec2_i& t_other) const;
+    bool operator < (const Vec2_i& t_other) const;
+    bool operator > (const Vec2_i& t_other) const;
+
+    int dot_product(const Vec2_i& t_other) const;
+    Vec2_i closs_product(const Vec2_i& t_other) const;
+    int get_magnitude() const;
   };
 
   void vec2_i_demo();
