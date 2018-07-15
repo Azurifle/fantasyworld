21feabfcc8d95761e3d50a4fa5d9ffc32ee55692
Nin Azurifle <dsirikasem@hotmail.com>
2018-07-15 21:55:21 +0700

    Setup ALUT
    include all necessary file

--- Demo_Center/Demo_list_menu.cpp
+++ Demo_Center/Demo_list_menu.cpp
@@ -71,8 +71,44 @@ namespace G6037599
 
   void Demo_list_menu::do_option()
   {
-    App app(640, 480, "My Game");
-    app.run();
+    //App app(640, 480, "My Game");
+    //app.run();
+
+    ALuint source;
+    ALenum error;
+    ALint status;
+
+    /* Create an AL buffer from the given sound file. */
+    ALuint buffer = alutCreateBufferFromFile("");
+    if (buffer == AL_NONE)
+    {
+      error = alutGetError();
+      fprintf(stderr, "Error loading file: '%s'\n",
+        alutGetErrorString(error));
+      alutExit();
+      exit(EXIT_FAILURE);
+    }
+
+    /* Generate a single source, attach the buffer to it and start playing. */
+    alGenSources(1, &source);
+    alSourcei(source, AL_BUFFER, buffer);
+    alSourcePlay(source);
+
+    /* Normally nothing should go wrong above, but one never knows... */
+    error = alGetError();
+    if (error != ALUT_ERROR_NO_ERROR)
+    {
+      fprintf(stderr, "%s\n", alGetString(error));
+      alutExit();
+      exit(EXIT_FAILURE);
+    }
+
+    /* Check every 0.1 seconds if the sound is still playing. */
+    do
+    {
+      alutSleep(0.1f);
+      alGetSourcei(source, AL_SOURCE_STATE, &status);
+    } while (status == AL_PLAYING);
   }
 
   void Demo_list_menu::back_to_main_menu()
